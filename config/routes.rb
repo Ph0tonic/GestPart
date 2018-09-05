971204f7ee9caa29922ea3c3361e76c99e2bfd2b
Bastien Wermeille <bastien.wermeille@gmail.com>
2018-09-05 22:36:20 +0200

    Implement dashboard + about page

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'welcome/index'
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#dashboard'
+  # For static pages
+  get "/:page" => "static#show"
 end
