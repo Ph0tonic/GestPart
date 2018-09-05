c05cd2e64be21aa2ff0933cc9d338e421932df5d
Bastien Wermeille <bastien.wermeille@gmail.com>
2018-09-05 21:50:31 +0200

    Add nav active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def current_class?(test_path)
+     return 'active ' if request.path == test_path
+     return''
+  end
+
+  def controllerActive?(*controller)
+    return 'active ' if controller.include?(params[:controller])
+    return ''
+  end
+
+  def controllerClass?(cssClass, *controller)
+    return cssClass if controller.include?(params[:controller])
+    return ''
+  end
 end
